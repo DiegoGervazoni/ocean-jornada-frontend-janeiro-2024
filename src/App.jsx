440ede5ebce31f851b9a1fbc120c8dcff4e2ff3f
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-01-24 14:28:39 -0300

    Criando card no App.jsx e personalizando container e card no app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="card">
+          <h2 className="name">Rick Sanchez</h2>
+          <div className="button-group">
+            <button className="character">Status: Vivo</button>
+            <button className="character">Espécie: Humana</button>
+          </div>
+          <button className="character">Origem: Terra C-137</button>
+          <img
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            alt="Imagem Rick Sanchez"
+            className="image"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
