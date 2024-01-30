9d1ed2fc0dfd474fb97e799229f5c9e0c44b5824
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-01-30 10:40:52 -0300

    Criação de outros cards com outros valores

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,23 @@
-function Card() {
+/* eslint-disable react/prop-types */
+function Card(props) {
+  // console.log(props);
+
+  const item = props.item;
+
+  //console.log(item);
+
   return (
     <>
       <div className="container">
         <div className="card">
-          <h2 className="name">Rick Sanchez</h2>
+          <h2 className="name">{item.name}</h2>
           <div className="button-group">
             <button className="character">Status: Vivo</button>
             <button className="character">Espécie: Humana</button>
           </div>
           <button className="character">Origem: Terra C-137</button>
           <img
-            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            src={item.image}
             alt="Imagem Rick Sanchez"
             className="image"
           />
